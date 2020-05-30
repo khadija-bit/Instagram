f861385920c893c0153bac3fe5bd4df0fe34d4a4
khadija-bit <sumeyahassan34@gmail.com>
2020-05-30 17:42:48 +0300

    add model class Image

--- instagram/models.py
+++ instagram/models.py
@@ -5,7 +5,7 @@ class Image(models.Model):
     Image = models.ImageField()
     name = models.CharField(max_length = 60)
     caption = models.CharField(max_length = 60)
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey()
     likes = models.ForeignKey()
     comments = models.ForeignKey()
     date = models.DateTimeField(auto_now_add=True)
