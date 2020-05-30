740ebb9d57c91778716d57d3112e3959f067af52
khadija-bit <sumeyahassan34@gmail.com>
2020-05-30 17:43:39 +0300

    add model class Image

--- instagram/models.py
+++ instagram/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    Image = models.ImageField()
+    image = models.ImageField()
     name = models.CharField(max_length = 60)
     caption = models.CharField(max_length = 60)
-    profile = models.ForeignKey()
+    profile = models.ForeignKey(e)
     likes = models.ForeignKey()
     comments = models.ForeignKey()
     date = models.DateTimeField(auto_now_add=True)
 
 
 
+class Profile(models.Model):
+    profile_photo
+    bio
