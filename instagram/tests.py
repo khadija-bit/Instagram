783ab15075f87a6246b0be52fb13bcf759f4d6b2
khadija-bit <sumeyahassan34@gmail.com>
2020-06-01 14:38:16 +0300

    Add Profile test

--- instagram/tests.py
+++ instagram/tests.py
@@ -14,3 +14,12 @@ class ImageTestClass(TestCase):
         self.assertTrue(isinstance(self.khadija,Image))    
 
 
+class ProfileTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.khadija = Profile(profile_photo = 'khadija')
+    
+    # Testing instance
+    def test_instance(self):    
+        self.assertTrue(isinstance(self.khadija,Profile))
