baea4396380f4f8ff684d6f54ebfd079207eb2b6
khadija-bit <sumeyahassan34@gmail.com>
2020-06-01 14:37:53 +0300

    Add image test

--- instagram/models.py
+++ instagram/models.py
@@ -5,11 +5,9 @@ class Image(models.Model):
     image = models.ImageField()
     name = models.CharField(max_length = 60)
     caption = models.CharField(max_length = 100)
-    profile = models.ForeignKey()
     likes = models.IntegerField()
-    comments = models.ForeignKey()
     date = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.name
 
@@ -27,20 +25,22 @@ class Profile(models.Model):
     profile_photo = models.ImageField()
     bio = models.CharField(max_length = 60)
     
+    
     def __str__(self):
-        return self.
+        return self.bio
 
     def save_profile(self):
-        self.save
+        self.save()
 
-    def delete_image(self):
-        self.delete
+    def delete_profile(self):
+        self.delete()
     
         
 
 class Comment(models.Model):
     comment = models.CharField(max_length = 100)
-    image = models.ForeignKey()
+    image = models.ForeignKey(Image)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.comment
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image,Profile,Comment
+
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.khadija = Image(image='khadija')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.khadija,Image))    
+
+
