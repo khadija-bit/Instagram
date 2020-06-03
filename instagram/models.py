8a41479bf363397d96ce0d9a37e5888915572cec
khadija-bit <sumeyahassan34@gmail.com>
2020-06-03 17:01:24 +0300

    Add styling

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     profile_photo = models.ImageField()
@@ -21,7 +21,7 @@ class Image(models.Model):
     caption = models.CharField(max_length = 100)
     likes = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -31,19 +31,38 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+    
+    def likes_total(self):
+        self.likes.count()
 
     @classmethod
     def search_image(cls,search_term):
         images = cls.objects.filter(name__icontains = search_term)
         return images
 
+    @classmethod
+    def all_comment(self):
+        return self.comment.all() 
+
 class Comment(models.Model):
     comment = models.CharField(max_length = 100)
     date = models.DateTimeField(auto_now_add=True)
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comment(cls):
+        comment = cls.objects.all()
+        return comment
+        
 
 class NewsLetterEnts(models.Model):
     name = models.CharField(max_length = 30)
